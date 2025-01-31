d1093f178dc107671356eef7599b75216f44f27b
zangula-dev <simalc@zangula.com>
2025-01-31 17:09:40 +0530

    Added SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    sitemap({
+      hostname: 'https://webbuildinfotech.com',
+      changefreq: 'daily',
+    }),
   ],
 })
